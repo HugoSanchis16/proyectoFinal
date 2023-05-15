807223b4c42a77b18d23f7b346c9306c2e5d49ee
HugoSanchis16 <99294677+HugoSanchis16@users.noreply.github.com>
2023-05-15 12:41:56 +0200

    Carpeta de proyecto añadida

---
+++ proyectoFinal/README.md
@@ -0,0 +1,4 @@
+# proyectoFinal
+Esto es el proyecto final del grupo 2. 1er DAW.
+Esta aplicaión consiste en lo siguiente: 
+
---
+++ proyectoFinal/src/com/proyectoFinal/grupo2/Usuario.java
@@ -0,0 +1,79 @@
+package com.proyectoFinal.grupo2;
+
+public class Usuario {
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private String imagen;
+	private String poblacion;
+	private String correoElectronico;
+	private String contrasena;
+	
+	public Usuario(int id, String nombre, String apellidos, String imagen, String poblacion, String correoElectronico,
+			String contrasena) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.imagen = imagen;
+		this.poblacion = poblacion;
+		this.correoElectronico = correoElectronico;
+		this.contrasena = contrasena;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getPoblacion() {
+		return poblacion;
+	}
+
+	public void setPoblacion(String poblacion) {
+		this.poblacion = poblacion;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+}
